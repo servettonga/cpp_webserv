0465a208dd1b48aac3a6c94be520a60bfec1f44c
servettonga <serveth@gmail.com>
2024-11-29 18:19:53 +0100

    Ref: Response

--- srcs/http/Response.hpp
+++ srcs/http/Response.hpp
@@ -6,7 +6,7 @@
 /*   By: sehosaf <sehosaf@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 20:07:58 by sehosaf           #+#    #+#             */
-/*   Updated: 2024/11/24 12:49:07 by sehosaf          ###   ########.fr       */
+/*   Updated: 2024/11/29 17:19:40 by sehosaf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,24 @@
 
 class Response {
 	private:
+		// Member variables
 		int _statusCode;
 		std::map<std::string, std::string> _headers;
 		std::string _body;
 
+		// Helper methods
 		void updateContentLength();
 		std::string getStatusText() const;
 
 	public:
-		Response(int statusCode = 200, const std::string &serverName = "webserv/1.1");
+		explicit Response(int statusCode = 200, const std::string &serverName = "webserv/1.1");
 		~Response();
 
 		void setStatusCode(int code);
-		void setBody(const std::string& body);
+		void setBody(const std::string &body);
+		void addHeader(const std::string &name, const std::string &value);
 
-		void addHeader(const std::string& name, const std::string& value);
-
-		std::string toString() const;
+		std::string toString() const;	// Generate response
 };
 
 #endif
